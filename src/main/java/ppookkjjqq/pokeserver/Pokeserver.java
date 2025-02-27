09131b79c9c79938efdd94658c3a2ddbcc416e5b
ppookkjjqq <a01077392387@gmail.com>
2025-02-28 08:40:31 +0900

    1test

---
+++ src/main/java/ppookkjjqq/pokeserver/Pokeserver.java
@@ -0,0 +1,17 @@
+package ppookkjjqq.pokeserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pokeserver extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
