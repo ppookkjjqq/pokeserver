30385ebb3726a224c5ca6cbd7222b65cb83c204d
ppookkjjqq <a01077392387@gmail.com>
2025-02-28 13:02:32 +0900

    1test

--- src/main/java/ppookkjjqq/pokeserver/Pokeserver.java
+++ src/main/java/ppookkjjqq/pokeserver/Pokeserver.java
@@ -1,17 +1,37 @@
 package ppookkjjqq.pokeserver;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import ppookkjjqq.pokeserver.controller.UserConnectionController;
+import ppookkjjqq.pokeserver.controller.UserManagementController;
 
 public final class Pokeserver extends JavaPlugin {
 
+    private static Pokeserver serverInstance;
+    private static UserManagementController userManagement;
+
     @Override
     public void onEnable() {
+        serverInstance = this;
+        userManagement = new UserManagementController();
+
         // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
+        serverInstance = null;
+        userManagement = null;
         // Plugin shutdown logic
     }
+
+    public static Pokeserver getServerInstance(){
+        return serverInstance;
+    }
+
+    public static UserManagementController getUserManagement(){
+        return userManagement;
+    }
+
+
 }
